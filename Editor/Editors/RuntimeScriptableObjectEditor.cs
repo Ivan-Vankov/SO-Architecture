9fa1704880e8a9de373b800866a6ca5462c4746b
Ivan-Vankov <ivanpvankov@gmail.com>
2023-01-25 10:26:29 +0200

    Added a RuntimeScriptableObject which resets on play mode exit

---
+++ Editor/Editors/RuntimeScriptableObjectEditor.cs
@@ -0,0 +1,17 @@
+﻿#if ODIN_INSPECTOR
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+
+namespace Vaflov {
+    [CustomEditor(typeof(RuntimeScriptableObject), true)]
+    public class RuntimeScriptableObjectEditor : OdinEditor {
+        public override void OnInspectorGUI() {
+            if (EditorApplication.isPlaying) {
+                SirenixEditorGUI.InfoMessageBox("Scriptable object will be reset on play mode exit");
+            }
+            base.OnInspectorGUI();
+        }
+    }
+}
+#endif
